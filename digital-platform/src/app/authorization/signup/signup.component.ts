412c6ebd03c6773bef3aa86d1a1a5d043247e115
strider <amgstrider@gmail.com>
2024-01-17 10:38:07 +0400

    feat: signup form validators

--- digital-platform/src/app/authorization/signup/signup.component.ts
+++ digital-platform/src/app/authorization/signup/signup.component.ts
@@ -1,24 +1,35 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthorizationService } from '../../core/authorization.service';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrl: './signup.component.scss',
 })
-export class SignupComponent implements OnDestroy, OnInit{
+export class SignupComponent implements OnDestroy, OnInit {
   signUpForm!: FormGroup;
 
   constructor(private authorizationService: AuthorizationService) {}
 
   ngOnInit(): void {
     this.signUpForm = new FormGroup({
-      email: new FormControl(this.authorizationService.getCurrentCredentials()[0], [Validators.required, Validators.email]),
-      password: new FormControl(this.authorizationService.getCurrentCredentials()[1], [Validators.required, Validators.minLength(6)]),
-      phoneNumber: new FormControl(null, Validators.required),
-      position: new FormControl('student')
-    })
+      email: new FormControl(
+        this.authorizationService.getCurrentCredentials()[0],
+        [Validators.required, Validators.email]
+      ),
+      password: new FormControl(
+        this.authorizationService.getCurrentCredentials()[1],
+        [Validators.required, this.passwordValidator]
+      ),
+      phoneNumber: new FormControl(null, [Validators.required, this.phoneValidator]),
+      position: new FormControl('student'),
+    });
 
     // this.signUpForm = new FormBuilder().group({
     //   email: [this.authorizationService.getCurrentCredentials()[0]],
@@ -28,6 +39,23 @@ export class SignupComponent implements OnDestroy, OnInit{
     // })
   }
 
+  passwordValidator(control: FormControl): { [s: string]: boolean } {
+    const passwordPattern =
+      '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+{}\\[\\]:;,.?/~\\\\-]).{8,}$';
+    if (control.value && !control.value.match(passwordPattern)) {
+      return { passwordError: true };
+    }
+    return null!;
+  }
+
+  phoneValidator(control: FormControl): { [s: string]: boolean } {
+    const phonePattern = '^[0-9]{10}$';
+    if (control.value && !control.value.match(phonePattern)) {
+      return { phoneError: true };
+    }
+    return null!;
+  }
+
   onSubmit() {
     console.log(this.signUpForm.value);
     this.signUpForm.reset();
@@ -35,8 +63,8 @@ export class SignupComponent implements OnDestroy, OnInit{
       email: '',
       password: '',
       phoneNumber: '',
-      position: 'student'
-    })
+      position: 'student',
+    });
   }
 
   ngOnDestroy(): void {
@@ -44,5 +72,5 @@ export class SignupComponent implements OnDestroy, OnInit{
       this.signUpForm.value.email,
       this.signUpForm.value.password
     );
-}
+  }
 }
