220d7de815bbeb84cb2c9eb3a239c2ad89e01327
strider <amgstrider@gmail.com>
2024-01-17 19:47:17 +0400

    feat: add authorizationService init test

--- digital-platform/src/app/core/authorization.service.spec.ts
+++ digital-platform/src/app/core/authorization.service.spec.ts
@@ -1,16 +1,7 @@
-import { TestBed } from '@angular/core/testing';
-
 import { AuthorizationService } from './authorization.service';
 
 describe('AuthorizationService', () => {
-  let service: AuthorizationService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthorizationService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('Shoul be initialized', () => {
+    expect(AuthorizationService).toBeTruthy();
   });
 });
