ae8553968c73ebdc3a9cb052a463e5183848fe22
strider <amgstrider@gmail.com>
2024-01-18 11:51:23 +0400

    feat: cover signup with tests

--- digital-platform/src/app/core/authorization.service.spec.ts
+++ digital-platform/src/app/core/authorization.service.spec.ts
@@ -41,14 +41,99 @@ describe('AuthorizationService', () => {
       localId: 'RtCCfg8tnIgCM862STqWhKGuQmm2',
     };
 
-    authorizationService.signUp('2@gmail.com', 'Password123!').subscribe(response => {
-      expect(response).toEqual(dummyResponse);
-    });
+    authorizationService
+      .signUp('2@gmail.com', 'Password123!')
+      .subscribe((response) => {
+        expect(response).toEqual(dummyResponse);
+      });
 
-    const request = httpMock.expectOne(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${ApiKey}`);
+    const request = httpMock.expectOne(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${ApiKey}`
+    );
 
     expect(request.request.method).toBe('POST');
 
     request.flush(dummyResponse);
+  });
+
+  it('should throw an error if the email already exists', () => {
+    const dummyErrorResponse = {
+      error: {
+        message: 'EMAIL_EXISTS',
+      },
+    };
+
+    const mockErrorStatus = { status: 400, statusText: 'Bad Request' };
+
+    authorizationService.signUp('2@gmail.com', 'Password123!').subscribe({
+      error: (error) => {
+        expect(error).toEqual('This email exists already');
+      },
+    });
+    const request = httpMock.expectOne(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${ApiKey}`
+    );
+
+    request.flush(dummyErrorResponse, mockErrorStatus);
+  });
+
+  it('Should throw an unknown error if error message not in the list', () => {
+    const dummyErrorResponse = {
+      error: {
+        message: 'NOT_IN_THE_LIST',
+      },
+    };
+
+    const mockErrorStatus = { status: 400, statusText: 'Bad Request' };
+
+    authorizationService.signUp('2@gmail.com', 'Password123!').subscribe({
+      error: (error) => {
+        expect(error).toEqual('An unknown error occurred!');
+      },
+    });
+
+    const request = httpMock.expectOne(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${ApiKey}`
+    );
+
+    request.flush(dummyErrorResponse, mockErrorStatus);
+  });
+
+  it('should twrow an unknow error if errorRes.error or errorRes.error.error is undefined', () => {
+    const dummyResponse = {
+      error: undefined,
+    }
+
+    const mockErrorStatus = { status: 400, statusText: 'Bad Request' };
+
+    authorizationService.signUp('email', 'password').subscribe({
+      error: (error) => {
+        expect(error).toEqual('An unknown error occurred!');
+      }
+    })
+
+    const request = httpMock.expectOne(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${ApiKey}`);
+
+    request.flush(dummyResponse, mockErrorStatus);
+  });
+
+  it('Should twrow an "Too many attempts, try again later" error if errorRes.error.error.message is "TOO_MANY_ATTEMPTS_TRY_LATER"', () => {
+    const dummyResponse = {
+        error: {
+          message: 'TOO_MANY_ATTEMPTS_TRY_LATER'
+      }
+    }
+
+    const mockErrorStatus = { status: 400, statusText: 'Bad Request' };
+
+    authorizationService.signUp('email', 'password').subscribe({
+      error: (error) => {
+        expect(error).toEqual('Too many attempts, try again later');
+      }
+    })
+
+    const request = httpMock.expectOne(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${ApiKey}`);
+
+    request.flush(dummyResponse, mockErrorStatus);
   });
 });
